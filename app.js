42a8ece16a41cd89a67f496c0906dcb9f0576d58
JuliusB-ui <141216908+JuliusB-ui@users.noreply.github.com>
2024-09-08 20:42:57 +0800

    done

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-// const express = require('express')
 import express from "express";
-// const logger = require('morgan')
 import logger from "morgan";
-// const cors = require('cors')
 import cors from "cors";
 
-// const contactsRouter = require('./routes/api/contacts')
-import {router as contactsRouter} from "./routes/api/contacts.js";
+import {router as contacts} from "./routes/api/contacts.js";
 
 const app = express()
 
@@ -16,13 +12,13 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/contacts', contactsRouter)
+app.use('/api/contacts', contacts)
 
-app.use((req, res) => {
+app.use((_req, res) => {
   res.status(404).json({ message: 'Not found' })
 })
 
-app.use((err, req, res, next) => {
+app.use((err, _req, res, _next) => {
   res.status(500).json({ message: err.message })
 })
 
